fb295bde81b02f96fe7675ccb7a386fd900b1c4f
yurakulinichenko <yurii.kulinichenko@intellectsoft.net>
2023-02-23 09:21:27 +0200

    final commit

---
+++ db/contacts.js
@@ -0,0 +1,47 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { nanoid } = require('nanoid');
+
+const contactsPath = path.join(__dirname, 'contacts.json');
+
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+};
+
+const getContactById = async (id) => {
+    const contactId = String(id);
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact || null;
+};
+
+const removeContact = async (id) => {
+    const contactId = String(id);
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    if (index === -1) return null;
+
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+};
+
+const addContact = async (data) => {
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        ...data,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+};
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-const contacts = require('./contacts');
+const contacts = require('./db/contacts');
 
-const argv = require('yargs').argv;
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
 
-function invokeAction({ action, id, name, email, phone }) {
+const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
         case 'list':
-            const contacts = await contacts.listContacts();
-            console.log(contacts);
-            break;
+            const list = await contacts.listContacts();
+            return console.table(list);
 
         case 'get':
-            const contact = await contacts.
-            break;
+            const contact = await contacts.getContactById(id);
+            return console.table(contact);
 
         case 'add':
-            // ... name email phone
-            break;
+            const newContact = await contacts.addContact({
+                name,
+                email,
+                phone,
+            });
+            return console.table(newContact);
 
         case 'remove':
-            // ... id
-            break;
+            const deletedContact = await contacts.removeContact(id);
+            return console.table(deletedContact);
 
         default:
             console.warn('\x1B[31m Unknown action type!');
     }
-}
+};
 
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
 invokeAction(argv);
